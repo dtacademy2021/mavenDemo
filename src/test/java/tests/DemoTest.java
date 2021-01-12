dc08b7982db2e5a62f9195125914df4ece4ec3de
Duotech Academy <duotechinstructor@gmail.com>
2021-01-11 22:14:47 -0500

    january11

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -1,12 +1,15 @@
 package tests;
 
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -16,6 +19,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class DemoTest {
@@ -27,7 +34,8 @@ public class DemoTest {
 		
 		System.out.println("Testing Maven");
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumFiles\\browserDrivers\\chromedriver.exe");
+
+		WebDriverManager.chromedriver().setup();
 		driver= new ChromeDriver();
 		
 		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
@@ -35,20 +43,32 @@ public class DemoTest {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("tes");
         driver.findElement(By.name("ctl00$MainContent$login_button")).click();
-        assertTrue(driver.getTitle().equals("Login"));
+        assertFalse(driver.getTitle().equals("Login"));
 //
 	}
 	
 	
-	@AfterMethod (alwaysRun = true)
-	public void teardownMethod() throws IOException {
-		// Based on the test result (pass, fail, skipped) you can put logic to take a screenshot
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-		FileUtils.copyFile(screenshot, new File("screenshots/screenshot"+ System.currentTimeMillis() +".png"));
+//	@AfterMethod (alwaysRun = true)
+//	public void teardownMethod() throws IOException {
+//		// Based on the test result (pass, fail, skipped) you can put logic to take a screenshot
+//		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+//		FileUtils.copyFile(screenshot, new File("screenshots/screenshot"+ System.currentTimeMillis() +".png"));
+//		
+//		driver.quit();
+//	}
+	
+	
+	@Test
+	public void test2() {
+		List<Integer> list = new ArrayList<>(); // To test Java version compatibility
 		
-		driver.quit();
+		Faker f = new Faker();
+		
+		System.out.println(f.internet().emailAddress());
+		
 	}
+	
 
 
 }
